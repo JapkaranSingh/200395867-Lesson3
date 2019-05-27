11baf0149a855a14278c03caa3c1a8659641d4cc
JapkaranSingh <japkaransingh2@gmail.com>
2019-05-27 15:58:53 -0400

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200395867_Lesson3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200395867_Lesson3.Startup))]
+namespace _200395867_Lesson3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
